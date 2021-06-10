f47b2fb356d611450b8a1272c761d8b96a78e6ca
AnyISalIn <anyisalin@gmail.com>
2021-06-10 16:33:25 +0800

    Add option for disable kube-proxy component
    Signed-off-by: AnyISalIn <anyisalin@gmail.com>

--- services/kubeproxy.go
+++ services/kubeproxy.go
@@ -2,13 +2,20 @@ package services
 
 import (
 	"context"
-
 	"github.com/rancher/rke/docker"
 	"github.com/rancher/rke/hosts"
 	v3 "github.com/rancher/rke/types"
+	"github.com/sirupsen/logrus"
+)
+
+const (
+	DisableKubeProxyLabel = "node-role.kubernetes.io/kube-proxy"
 )
 
 func runKubeproxy(ctx context.Context, host *hosts.Host, df hosts.DialerFactory, prsMap map[string]v3.PrivateRegistry, kubeProxyProcess v3.Process, alpineImage string) error {
+	if checkKubeProxyDisable(host) {
+		return nil
+	}
 	imageCfg, hostCfg, healthCheckURL := GetProcessConfig(kubeProxyProcess, host)
 	if err := docker.DoRunContainer(ctx, host.DClient, imageCfg, hostCfg, KubeproxyContainerName, host.Address, WorkerRole, prsMap); err != nil {
 		return err
@@ -20,9 +27,26 @@ func runKubeproxy(ctx context.Context, host *hosts.Host, df hosts.DialerFactory,
 }
 
 func removeKubeproxy(ctx context.Context, host *hosts.Host) error {
+	if checkKubeProxyDisable(host) {
+		return nil
+	}
 	return docker.DoRemoveContainer(ctx, host.DClient, KubeproxyContainerName, host.Address)
 }
 
 func RestartKubeproxy(ctx context.Context, host *hosts.Host) error {
+	if checkKubeProxyDisable(host) {
+		return nil
+	}
 	return docker.DoRestartContainer(ctx, host.DClient, KubeproxyContainerName, host.Address)
+}
+
+func checkKubeProxyDisable(host *hosts.Host) bool {
+	logrus.Warn(host.Labels)
+	if v, ok := host.Labels[DisableKubeProxyLabel]; ok {
+		if v == "false" {
+			return true
+		}
+	}
+	logrus.Warnf("host [%s] doesn't need kube-proxy", host.NodeName)
+	return false
 }
