219b2f89eeedd2602188d16991f5700ccb0ccd22
AnyISalIn <anyisalin@gmail.com>
2021-06-10 16:33:25 +0800

    Add option for disable kube-proxy component

--- services/kubeproxy.go
+++ services/kubeproxy.go
@@ -8,7 +8,14 @@ import (
 	v3 "github.com/rancher/rke/types"
 )
 
+const (
+	DisableKubeProxyLabel = "node-role.kubernetes.io/kube-proxy"
+)
+
 func runKubeproxy(ctx context.Context, host *hosts.Host, df hosts.DialerFactory, prsMap map[string]v3.PrivateRegistry, kubeProxyProcess v3.Process, alpineImage string) error {
+	if checkKubeProxyDisable(host) {
+		return nil
+	}
 	imageCfg, hostCfg, healthCheckURL := GetProcessConfig(kubeProxyProcess, host)
 	if err := docker.DoRunContainer(ctx, host.DClient, imageCfg, hostCfg, KubeproxyContainerName, host.Address, WorkerRole, prsMap); err != nil {
 		return err
@@ -20,9 +27,24 @@ func runKubeproxy(ctx context.Context, host *hosts.Host, df hosts.DialerFactory,
 }
 
 func removeKubeproxy(ctx context.Context, host *hosts.Host) error {
+	if checkKubeProxyDisable(host) {
+		return nil
+	}
 	return docker.DoRemoveContainer(ctx, host.DClient, KubeproxyContainerName, host.Address)
 }
 
 func RestartKubeproxy(ctx context.Context, host *hosts.Host) error {
+	if checkKubeProxyDisable(host) {
+		return nil
+	}
 	return docker.DoRestartContainer(ctx, host.DClient, KubeproxyContainerName, host.Address)
+}
+
+func checkKubeProxyDisable(host *hosts.Host) bool {
+	if v, ok := host.Labels[DisableKubeProxyLabel]; ok {
+		if v == "false" {
+			return true
+		}
+	}
+	return false
 }
